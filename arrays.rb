84df78c9c2fce7a74db81d2b0682b3acb0ed600f
Shawn Lee <Shawnl93@hotmail.com>
2022-06-08 11:38:17 -0600

    Declared 4 different variables with different Arrays

---
+++ arrays.rb
@@ -0,0 +1,10 @@
+# Learning Arrays
+
+Emails = ["email1@turing.com", "email2@turing.com", "email3@tur
+ing.com", "email4@turing.com"]
+
+Ages = [20, 21, 22 ,23]
+
+Lunch_cost = [8.90, 12.22, 24.30 , 4.30]
+
+Hungry =[true, false, false, true]
