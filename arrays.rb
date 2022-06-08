6fe961d82af511caf19d958d933cffc9f16e3f60
Shawn Lee <Shawnl93@hotmail.com>
2022-06-08 11:47:17 -0600

    Add an extra element for each different array

--- arrays.rb
+++ arrays.rb
@@ -8,3 +8,19 @@ Ages = [20, 21, 22 ,23]
 Lunch_cost = [8.90, 12.22, 24.30 , 4.30]
 
 Hungry =[true, false, false, true]
+
+# This will remove the last email
+
+Emails.pop
+
+#This will remove the first Ages
+
+Ages.shift
+
+#This will add another Lunch Lunch_cost in the beginning
+
+Lunch_cost.unshift(27.35)
+
+#This will add another false at the end
+
+Hungry.push(true)
